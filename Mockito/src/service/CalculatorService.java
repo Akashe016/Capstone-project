937a87ff8cdc84395ef457dbb450e3c696f096f5
Siddharthan <siddharthan.anbazhagan@gmail.com>
2024-06-08 11:11:58 +0530

    file added

---
+++ Mockito/src/service/CalculatorService.java
@@ -0,0 +1,9 @@
+package service;
+
+public interface CalculatorService {
+
+	int add(int a, int b);
+    int multiply(int a, int b);
+    int sub(int a, int b);
+    double divide(double d1, double d2);
+}
